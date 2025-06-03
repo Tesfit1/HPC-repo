778b96d5c1f688998840a484a66e3a42a6681a6d
tesfit <tesfitgebremeskel@gmail.com>
2025-06-03 14:24:14 +0200

    removed cached

--- src/dateConv.py
+++
@@ -1,7 +0,0 @@
-
-from datetime import datetime
-def convert_date_format(date_str):
-        try:
-            return datetime.strptime(date_str, "%d %b %Y").strftime("%Y-%m-%d")
-        except (ValueError, TypeError):
-            return None  # Handle missing or invalid dates
